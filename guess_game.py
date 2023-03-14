002acd52f13b50aa07158f6e9884d6dce33d5549
jon <maybetomorrow789@gmail.com>
2023-03-14 21:07:09 +0800

    修改優化程式

--- guess_game.py
+++ guess_game.py
@@ -10,7 +10,7 @@ print(x)
 win = False
 start, end = 1, 50
 for i in range(5):
-    y = eval(input(f'{i+1}/5請猜一個數字(1~50之間):'))
+    y = eval(input(f'{i+1}/5請猜一個數字({start}~{end}):'))
     if x == y:
         win = True
         break
@@ -18,11 +18,11 @@ for i in range(5):
     elif x > y:
         if start < y:
             start = y
-        print(f'猜大一點{start}~{end}')
+        print(f'猜大高一點')
     else:
         if end > y:
             end = y
-        print(f'猜小一點{start}~{end}')
+        print(f'猜小一點')
 
 if win:
     print(f'恭喜過關!總共猜了:{i+1}次')
